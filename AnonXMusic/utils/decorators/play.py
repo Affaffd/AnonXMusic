48ce757f166afbc50c3eebd4cdfced76e9697ace
Affaffd <122824167+Affaffd@users.noreply.github.com>
2023-12-18 22:20:02 +0300

    Add files via upload
    Signed-off-by: Affaffd <122824167+Affaffd@users.noreply.github.com>

--- AnonXMusic/utils/decorators/play.py
+++ AnonXMusic/utils/decorators/play.py
@@ -29,21 +29,6 @@ links = {}
 
 def PlayWrapper(command):
     async def wrapper(client, message):
-        language = await get_lang(message.chat.id)
-        _ = get_string(language)
-        if message.sender_chat:
-            upl = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="AnonymousAdmin",
-                        ),
-                    ]
-                ]
-            )
-            return await message.reply_text(_["general_3"], reply_markup=upl)
-
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
@@ -54,8 +39,9 @@ def PlayWrapper(command):
         try:
             await message.delete()
         except:
-            pass
-
+                pass
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
         audio_telegram = (
             (message.reply_to_message.audio or message.reply_to_message.voice)
             if message.reply_to_message
